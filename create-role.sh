ceb637582242688db7d2e6dfcfa29357739c2c2d
Ubuntu <patrickchenpy@gmail.com>
2021-07-12 23:58:59 +0000

    update creat-role

--- create-role.sh
+++ create-role.sh
@@ -3,6 +3,7 @@ read role
 
 echo "Creating new role: $role ..."
 
+cd "G10-TEMPLATES"
 cd "roles"
 
 mkdir $role
@@ -15,13 +16,15 @@ cd ".."
 
 mkdir "templates"
 cd "templates"
-touch "router.j2"
+touch "config.j2"
 cd ".."
 
 mkdir "vars"
 cd "vars"
 touch "main.yaml"
+cd ".."
 
+cd ".."
 cd "../../G10-CONFIGS"
 mkdir $role
 cd ".."
